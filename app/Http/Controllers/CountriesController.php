f14d1271516f52d016f39f1ca4a795573f85446b
EchoBig <ninebig1990@gmail.com>
2021-08-31 09:12:42 +0700

    First Init

---
+++ app/Http/Controllers/CountriesController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Country;
+use DataTables;
+
+class CountriesController extends Controller
+{
+    
+    public function index(){
+        return view('countries.list');
+    }
+
+    // Save Data
+    public function addCountry(Request $request){
+        $validator = \Validator::make($request->all(),[
+            'country_name'  => 'required|unique:countries',
+            'capital_city'  => 'required',
+        ]);
+
+        if (!$validator->passes()) {
+            return response()->json(['code'=>0,'error'=>$validator->errors()->toArray()]);
+        }else{
+            $country = new Country();
+            $country->country_name  = $request->country_name;
+            $country->capital_city  = $request->capital_city;
+
+            $query = $country->save();
+
+            if (!$query) {
+                return response()->json(['code'=>0,'msg'=>'cannot save data!']);
+            }else{
+                return response()->json(['code'=>1,'msg'=>'Saving data complete!']);
+            }
+        }
+    }
+
+    // Get Data
+    public function getCountriesList(){
+        $countries = Country::all();
+
+        return DataTables::of($countries)
+                            ->addIndexColumn()
+                            ->addColumn('action',function($row){
+                                return '<div class="btn-group">
+                                            <button class="btn btn-sm btn-primary" data-id="'.$row['id'].'">Update</button>
+                                            <button class="btn btn-sm btn-warning">Delete</button>
+                                        </div>';
+                            })
+                            ->make(true);
+    }
+}
---
+++ resources/views/countries/list.blade.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<title>List Countries</title>
+	<link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('datatable/css/dataTables.bootstrap.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('datatable/css/dataTables.bootstrap4.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('sweetalert2/sweetalert2.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('toastr/toastr.min.css') }}">
+</head>
+<body>
+	<div class="container">
+		<div class="row mt-5">
+			<div class="col-md-8 mb-2">
+				<div class="card">
+					<div class="card-header">
+						List Countries
+					</div>
+					<div class="card-body">
+						<table class="table table-hover table-sm" id="contries-table">
+							<thead>
+								<th>#</th>
+								<th>Country Name</th>
+								<th>Capital City</th>
+								<th>Action</th>
+							</thead>
+							<tbody>
+								
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-header">
+						Insert New Country
+					</div>
+					<div class="card-body">
+						<form action="{{ route('add.country') }}" method="POST" id="add-country">
+							@csrf
+							<div class="form-group">
+								<label for="country_name">Country Name</label>
+								<input type="text" name="country_name" class="form-control">
+								<span class="text-danger error-text country_name_error"></span>
+							</div>
+							<div class="form-group">
+								<label for="capital_city">Capital Name</label>
+								<input type="text" name="capital_city" class="form-control">
+								<span class="text-danger error-text capital_city_error"></span>
+							</div>
+							<div class="form-group">
+								<button class="btn btn-primary btn-block">Save</button>
+							</div>
+
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<script src="{{ asset('jquery/jquery-3.6.0.min.js') }}"></script>
+	<script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
+	<script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+	<script src="{{ asset('datatable/js/jquery.dataTables.min.js') }}"></script>
+	<script src="{{ asset('datatable/js/dataTables.bootstrap4.min.js') }}"></script>
+	<script src="{{ asset('sweetalert2/sweetalert2.min.js') }}"></script>
+	<script src="{{ asset('toastr/toastr.min.js') }}"></script>
+
+	<script>
+		toastr.options.preventDuplicates = true;
+
+		$.ajaxSetup({
+			headers:{
+				'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+			}
+		});
+
+		$(function(){
+
+			$('#add-country').on('submit',function(e){
+				e.preventDefault();
+
+				var form = this;
+
+				$.ajax({
+					url:$(form).attr('action'),
+					method:$(form).attr('method'),
+					data:new FormData(form),
+					processData:false,
+					dataType:'json',
+					contentType:false,
+					beforeSend:function(){
+						$(form).find('span.error-text').text('');
+					},
+					success:function(data){
+						if (data.code == 0) {
+							$.each(data.error,function(prefix,val){
+								$(form).find('span.'+prefix+'_error').text(val[0]);
+							});
+						}else{
+							$(form)[0].reset();
+							$('#contries-table').DataTable().ajax.reload(null,false);
+							toastr.success(data.msg);
+						}
+					},
+					error:function(e){
+						console.log(e);
+					}
+				});
+			});
+
+			// Get Countries
+			$('#contries-table').DataTable({
+				processing:true,
+				info:true,
+				ajax:"{{ route('get.contyies.list') }}",
+				columns:[
+					// {data:'id',name:'id'},
+					{data:"DT_RowIndex",name:"DT_RowIndex"},// ระบุให้เรียงลำดับใหม่โดยไม่สนลำดับในฐานข้อมูล
+					{data:"country_name",name:"country_name"},
+					{data:"capital_city",name:"capital_city"},
+					{data:"action",name:"action"}
+				]
+			});
+		});
+	</script>
+
+</body>
+</html>
